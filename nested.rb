208496bcadb89bbc80ec9c50903b7b6d0a9d9cb9
Jacob W Jones <jacobwadejones76@gmail.com>
2020-11-09 18:28:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,9 +81,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_motsumoto] = {}
-  programmer_hash[:yukihiro_motsumoto][:known_for] = "Ruby"
-  programmer_hash[:yukihiro_motsumoto][:languages] = ["LISP", "C"]
+  programmer_hash[:yukihiro_matsumoto] = {}
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
   programmer_hash
 
     
